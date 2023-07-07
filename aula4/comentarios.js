5222c2421718bc3fd5355e57234d5a0aa0d770db
Cleber Canto <cantosaraiva@hotmail.com>
2023-07-07 06:53:48 -0400

    first commit

---
+++ aula4/comentarios.js
@@ -0,0 +1,10 @@
+/*
+Comentário de block
+Qualquer texto
+*/
+console.log('Olá Mundo!');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Comentário de linha
